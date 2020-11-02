f1eb44ff8d08178b46a1738562ef603514ef14d4
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-11-02 10:44:42 +0530

    Update ytdl.py

--- stdplugins/ytdl.py
+++ stdplugins/ytdl.py
@@ -220,9 +220,17 @@ async def download_video(v_url):
 
 @borg.on(admin_cmd("song ?(.*)"))
 async def _(event):
-    song = event.pattern_match.group(1)
-    if not song:
+    args = event.pattern_match.group(1)
+    if not args:
         await event.edit("`Enter song name`")
         return
-    result = await event.client.inline_query("lybot", song)
-    await result[0].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
+    chat = -1001271479322
+    current_chat = event.chat_id
+    current_msg = event.id
+    try:
+       async for event in event.client.iter_messages(chat, search=args, limit=1, filter=InputMessagesFilterMusic):
+                    await event.client.send_file(current_chat, event, caption=event.message)
+    except:
+             await event.reply("`Song not found.")
+             return
+    await event.client.delete_messages(current_chat, current_msg)
